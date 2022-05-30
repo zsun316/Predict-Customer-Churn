ad75b169b5c128037bfc41d1db0a3f020b66dc8c
Zhaohan Sun <szh6615nku@gmail.com>
2022-05-30 16:16:45 -0400

    modify test funcs for import_data and perform_eda

--- churn_script_logging_and_tests.py
+++ churn_script_logging_and_tests.py
@@ -1,6 +1,6 @@
 import os
 import logging
-import churn_library_solution as cls
+import churn_library as cl
 
 logging.basicConfig(
     filename='./logs/churn_library.log',
@@ -10,7 +10,7 @@ logging.basicConfig(
 
 def test_import(import_data):
 	'''
-	test data import - this example is completed for you to assist with the other test functions
+	test data import 
 	'''
 	try:
 		df = import_data("./data/bank_data.csv")
@@ -27,10 +27,54 @@ def test_import(import_data):
 		raise err
 
 
-def test_eda(perform_eda):
+
+def test_eda(import_data, perform_eda):
 	'''
 	test perform eda function
 	'''
+
+	df = import_data("./data/bank_data.csv")
+	perform_eda(df)
+	pth_churn = './images/eda/churn_distribution.png'
+	pth_age = './images/eda/customer_age_distribution.png'
+	pth_heatmap = './images/eda/heatmap.png'
+	pth_marital = './images/eda/marital_status_distribution.png'
+	pth_transaction = './images/eda/total_transaction_distribution.png'
+
+	try:
+		assert os.path.exists(pth_churn) == True and os.path.getsize(pth_churn) > 0
+		logging.info("Check churn_distribution.png: SUCCESS")
+	except AssertionError as err:
+		logging.error("Check churn_distribution.png: The file wasn't found")
+		raise err
+
+	try:
+		assert os.path.exists(pth_age) == True and os.path.getsize(pth_age) > 0
+		logging.info("Check customer_age_distribution.png: SUCCESS")
+	except AssertionError as err:
+		logging.error("Check customer_age_distribution.png: The file wasn't found")
+		raise err
+
+	try:
+		assert os.path.exists(pth_heatmap) == True and os.path.getsize(pth_heatmap) > 0
+		logging.info("Check heatmap.png: SUCCESS")
+	except AssertionError as err:
+		logging.error("Check heatmap.png: The file wasn't found")
+		raise err
+
+	try:
+		assert os.path.exists(pth_marital) == True and os.path.getsize(pth_marital) > 0
+		logging.info("Check marital_status_distribution.png: SUCCESS")
+	except AssertionError as err:
+		logging.error("Check marital_status_distribution.png: The file wasn't found")
+		raise err
+
+	try:
+		assert os.path.exists(pth_transaction) == True and os.path.getsize(pth_transaction) > 0
+		logging.info("Check total_transaction_distribution.png: SUCCESS")
+	except AssertionError as err:
+		logging.error("Check total_transaction_distribution.png: The file wasn't found")
+		raise err
 
 
 def test_encoder_helper(encoder_helper):
@@ -52,7 +96,9 @@ def test_train_models(train_models):
 
 
 if __name__ == "__main__":
-	pass
+	test_import(cl.import_data)
+	test_eda(cl.import_data, cl.perform_eda)
+
 
 
 
